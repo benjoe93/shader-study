2d1cfba4d7c9e580b0f5d41cba44b2357f9c9e7a
benjoe93 <blazerbence@gmail.com>
2026-08-12 21:20:03 +0100

    Template update

--- _templates/_blank/shaders/fragment.glsl
+++ _templates/_blank/shaders/fragment.glsl
@@ -4,6 +4,23 @@ uniform float uTime;
 uniform vec2 uResolution;
 uniform vec2 uMouse;
 
+float inverseLerp(float v, float minValue, float maxValue);
+float remap(float v, float inMin, float inMax, float outMin, float outMax);
+
+
 void main() {
-  gl_FragColor = vec4(vUv, sin(uTime) * 0.5 + 0.5, 1.0);
+  vec3 color = vec3(1.0);
+
+  gl_FragColor = vec4(color, 1.0);
 }
+
+
+float inverseLerp(float v, float minValue, float maxValue) {
+  return (v - minValue) / (maxValue - minValue);
+}
+
+float remap(float v, float inMin, float inMax, float outMin, float outMax) {
+  float t = inverseLerp(v, inMin, inMax);
+  return mix(outMin, outMax, t);
+}
+
